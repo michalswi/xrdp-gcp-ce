0ceef72ff7b55343ce48f97b0b528d053ffa642b
ms <michal.swierczewski@nokia.com>
2024-03-13 13:42:49 +0100

    desc update

--- output.tf
+++ output.tf
@@ -1,12 +1,14 @@
-
 output "vm_pip" {
-  value = google_compute_address.static_ip.address
+  value       = google_compute_address.static_ip.address
+  description = "The public IP address of the virtual machine."
 }
 
 output "user" {
-  value = var.user
+  value       = var.user
+  description = "The username used for the virtual machine."
 }
 
 output "ssh" {
-  value = join("", ["ssh -i ", local_file.ssh_private_key.filename, " ", var.user, "@", google_compute_address.static_ip.address, " -vv"])
+  value       = join("", ["ssh -i ", local_file.ssh_private_key.filename, " ", var.user, "@", google_compute_address.static_ip.address, " -vv"])
+  description = "The SSH command to connect to the virtual machine."
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "name" {
-  type    = string
-  default = "rdp"
+  type        = string
+  default     = "rdp"
+  description = "The name to be used for resources that will be created."
 }
 
 variable "project" {
-  type = string
+  type        = string
+  description = "The ID of the project in which resources will be managed."
 }
 
 variable "region" {
-  type    = string
-  default = "us-central1"
+  type        = string
+  default     = "us-central1"
+  description = "The region in which resources will be created."
 }
 
 variable "user" {
-  type = string
+  type        = string
+  description = "The username to be used for the resources."
 }
